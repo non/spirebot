5a2eccc1b560f64c76bb99863a79401d9c45780a
Erik Osheim <d_m@plastic-idolatry.com>
2013-08-30 00:56:48 -0400

    add example script for running spirebot

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# sample script to run spirebot
+
+# we have to ask for UTF-8 in so many places
+export LCLANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+ASSEMBLY=target/scala-2.10/spirebot-assembly-0.4.jar
+
+if [ ! -r $ASSEMBLY ]; then
+    echo "couldn't find $ASSEMBLY"
+    echo "did you run 'sbt assembly' yet?"
+    exit 1
+fi
+
+java \
+ -Dnick=spirebot__ \
+ -Downer=d_m \
+ -Dserver=irc.freenode.net \
+ -Dchannels='#spirebot-test' \
+ -Dsun.jnu.encoding=UTF-8 \
+ -Dfile.encoding=UTF-8 \
+ -cp $ASSEMBLY \
+ org.spirebot.Spirebot

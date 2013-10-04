b8aaaac863f69d029ecf0c4c0a1d4376fe14574a
Erik Osheim <d_m@plastic-idolatry.com>
2013-10-04 01:53:49 -0400

    added timer methods

--- src/main/scala/spirebot/Util.scala
+++ src/main/scala/spirebot/Util.scala
@@ -1,6 +1,26 @@
 package spirebot
 
+import ichi.bench.Thyme
+
 object Util {
+  val th = new Thyme(watchLoads = false, watchGarbage = false, watchMemory = false)
+
+  def disp(t: Long): String =
+    if (t < 1000) "%dns" format t
+    else if (t < 1000000) "%.1fµs" format (t / 1000.0)
+    else if (t < 1000000000) "%.2fms" format (t / 1000000.0)
+    else "%.3fs" format (t / 1000000000.0)
+
+  def fmt(n: Double): String = disp((n * 1000000000).toLong)
+
+  def timer[A](f: => A): A = {
+    val br = Thyme.Benched.empty
+    val a = th.bench(f)(br, effort = 1)
+    val (m, r, e) = (fmt(br.runtime), br.runtimeIterations, fmt(br.runtimeError))
+    System.out.println(s"averaged $m over $r runs (± $e)")
+    a
+  }
+
   def setting(name: String): Option[String] =
     Option(System.getProperty(name))
 
